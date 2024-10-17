f599feb9a1a8a6dc10ca4efc6610828def57c224
Ale <alejaman04@gmail.com>
2024-10-17 12:26:03 -0400

    added waves1 component and put it in root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Navbar from "@/components/Navbar";
+import Waves1 from "@/assets/Waves1.jsx"
 export default function Home() {
   return (
     <>
       <Navbar />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat eligendi deleniti quam? Earum, eveniet cumque quam, officia aliquam doloremque illum qui harum quas, saepe nisi nulla ex error at facilis.</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum quo et ipsa molestiae praesentium corporis recusandae, perspiciatis quaerat reprehenderit vel dolorem delectus deleniti assumenda laudantium nesciunt omnis consectetur maiores. Autem!</p>
+      <Waves1 />
     </>
   );
 }
