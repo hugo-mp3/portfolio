6462a6b7e535c17d4c96ae7c358060c25f23149e
Ale <alejaman04@gmail.com>
2024-10-18 00:34:10 -0400

    overlapped animated background for about me section and added it to root page

--- app/dashboard/AboutMe.jsx
+++ app/dashboard/AboutMe.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React,{useEffect, useState} from 'react';
 import dynamic from 'next/dynamic'
 
@@ -14,10 +15,18 @@ export default function AboutMe(){
 
     return(
         <>
-            <div className=''>
-
+            <div className='w-full absolute'>
+                <Player
+                autoplay
+                loop
+                src="https://lottie.host/57eedb65-da8a-49c7-a64a-4c56748a74ce/BVUH8QwboT.json"
+                style={{ height: '400px', width: '100%'}}
+                className="z-0 object-cover"
+                />
             </div>
+            <div className='z-10 relative text-center pt-10'>
             <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">About Me</h3>
+            </div>
 
         </>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Navbar from "@/components/Navbar";
 import Waves1Down from "@/assets/Waves1Down.jsx"
 import Intro from "@/app/dashboard/Intro.jsx"
+import AboutMe from "@/app/dashboard/AboutMe.jsx"
 export default function Home() {
   return (
     <>
       <Navbar />
       <Intro />
       <Waves1Down />
-      
+      <AboutMe />
     </>
   );
 }
