f27ad11122fb9305f4397e57fcceeff757c9027b
Ale <alejaman04@gmail.com>
2024-10-17 23:47:02 -0400

    changed comment to be more accurate specific, Player is imported only on client side

--- app/dashboard/Intro.jsx
+++ app/dashboard/Intro.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import {Button} from "@/components/ui/button.tsx"
 import {ReaderIcon} from "@radix-ui/react-icons"
 import dynamic from 'next/dynamic';
-// Dynamically import the Player component
+// Dynamically import the Player component only on the client side
 const Player = dynamic(() => import('@lottiefiles/react-lottie-player').then(mod => mod.Player), {
     ssr: false
   });
