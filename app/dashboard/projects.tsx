3c9196253b34cf7f2cf5e5a65168c55092b71fe9
Ale <alejaman04@gmail.com>
2024-10-19 13:36:47 -0400

    added card to projects

--- app/dashboard/projects.tsx
+++ app/dashboard/projects.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
 
 export default function Projects(){
     return(
         <>
             <div className='text-center relative pt-10 z-10'>
                 <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Projects</h3>
-                
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Card Title</CardTitle>
+                        <CardDescription>Card Description</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <p>Card Content</p>
+                    </CardContent>
+                    <CardFooter>
+                        <p>Card Footer</p>
+                    </CardFooter>
+                </Card>
+
             </div>
         </>
     )
