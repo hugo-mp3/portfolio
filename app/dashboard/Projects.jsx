b73147529091289d52d31f9bceaf2fcce03564f1
Ale <alejaman04@gmail.com>
2024-10-18 21:28:52 -0400

    added header to projects component and added projects component to root page

--- app/dashboard/Projects.jsx
+++ app/dashboard/Projects.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Projects(){
     return(
         <>
-            
+            <div className='text-center relative pt-10 z-10'>
+                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Projects</h3>
+            </div>
         </>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Waves1Down from "@/assets/Waves1Down.jsx"
 import Intro from "@/app/dashboard/Intro.jsx"
 import AboutMe from "@/app/dashboard/AboutMe.jsx"
 import SkillsAndTools from "@/app/dashboard/SkillsAndTools.jsx"
+import Projects from "@/app/dashboard/Projects.jsx"
 export default function Home() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function Home() {
       <Waves1Down />
       <AboutMe />
       <SkillsAndTools />
+      <Projects />
     </>
   );
 }
