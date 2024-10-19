464abe92aa1ecd9670d95c5686f0c3eccc47ee9e
Ale <alejaman04@gmail.com>
2024-10-19 15:39:28 -0400

    added image to manipulate card and make it link and aligned header better

--- app/dashboard/projects.tsx
+++ app/dashboard/projects.tsx
@@ -15,14 +15,14 @@ export default function Projects(){
         <>
             <div className='w-full text-center pt-10 z-10'>
                 <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Projects</h3>
-                <div className='flex items-center justify-center w-full text-center z-10'>
-                    <Card className='flex w-3/4 m-10'>
-                        <CardHeader>
+                <div className='flex items-center content-center justify-center w-full text-center z-10'>
+                    <Card className='flex w-1/2 m-10 justify-between'>
+                        <CardHeader className='flex justify-center text-center'>
                             <CardTitle>Manipulate AI</CardTitle>
                             <CardDescription>A platform and SaaS solution that provides users with a personalized AI mentor for guidance and manipulation.</CardDescription>
                         </CardHeader>
                         <CardContent className='p-6 content-center'>
-                            <p>Card Content</p>
+                            <a href="https://www.manipulate.app/"><img src='/images/manipulateai.png'width={600}/></a>
                         </CardContent>
                     </Card>
                 </div>
