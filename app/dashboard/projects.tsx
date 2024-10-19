857de29efe11bf3e7d933caa7d60d768f0c1434e
Ale <alejaman04@gmail.com>
2024-10-19 18:02:09 -0400

    flex wrapped tools used for manipulate ai and have working version but will restyle cuz i think its kinda ugly

--- app/dashboard/projects.tsx
+++ app/dashboard/projects.tsx
@@ -16,12 +16,31 @@ export default function Projects(){
             <div className='w-full text-center pt-10 z-10'>
                 <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Projects</h3>
                 <div className='flex items-center content-center justify-center w-full text-center z-10'>
-                    <Card className='flex w-1/2 m-10 justify-between border-accent-border hover:drop-shadow-xs hover:scale-105'>
+                    <Card className='w-1/2 m-10 justify-between border-accent-border hover:drop-shadow-xs hover:scale-105'>
                         <CardHeader className='flex justify-center text-center'>
                             <CardTitle>Manipulate AI</CardTitle>
-                            <CardDescription>A platform and SaaS solution that provides users with a personalized AI mentor for guidance and manipulation.</CardDescription>
+                            <CardDescription className='text-primary'>A platform and SaaS solution that provides users with a personalized AI mentor for guidance and manipulation.</CardDescription>
+                            <div className="flex flex-wrap h-5 items-center justify-center space-x-4 text-sm text-muted-foreground">
+                                <div>Next.js</div>
+                                <Separator orientation="vertical" />
+                                <div>React.js</div>
+                                <Separator orientation="vertical" />
+                                <div>Shadcn</div>
+                                <Separator orientation="vertical" />
+                                <div>Vercel</div>
+                                <Separator orientation="vertical" />
+                                <div>TailwindCSS</div>
+                                <Separator orientation="vertical" />
+                                <div>FastAPI</div>
+                                <Separator orientation="vertical" />
+                                <div>MongoDB</div>
+                                <Separator orientation="vertical" />
+                                <div>OpenAI</div>
+                                <Separator orientation="vertical" />
+                                <div>AWS</div>
+                            </div>
                         </CardHeader>
-                        <CardContent className='p-6 content-center'>
+                        <CardContent className='flex pb-6 pl-6 pr-6 content-center justify-center'>
                             <a href="https://www.manipulate.app/"><img src='/images/manipulateai.png'width={600}/></a>
                         </CardContent>
                     </Card>
