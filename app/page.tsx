3891abec56d49397c9c991e728a0d1dfd2b99332
Ale <alejaman04@gmail.com>
2024-10-18 22:45:59 -0400

    changed names and tried overflow hidden on aboutme but it didnt work

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Navbar from "@/components/Navbar";
 import Waves1Down from "@/assets/Waves1Down.jsx"
-import Intro from "@/app/dashboard/Intro.jsx"
-import AboutMe from "@/app/dashboard/AboutMe.jsx"
-import SkillsAndTools from "@/app/dashboard/SkillsAndTools.jsx"
-import Projects from "@/app/dashboard/Projects.jsx"
+import Intro from "@/app/dashboard/intro.jsx"
+import AboutMe from "@/app/dashboard/aboutme.jsx"
+import SkillsAndTools from "@/app/dashboard/skillsandtools.jsx"
+import Projects from "@/app/dashboard/projects.jsx"
 export default function Home() {
   return (
     <>
