a893a094495ad05b3545d475f35b5c5463f68e15
Ale <alejaman04@gmail.com>
2024-10-21 19:17:46 -0400

    fixed issue with hugobot translate text shaking and made minor adjustments to both cards margins for text

--- app/dashboard/projects.tsx
+++ app/dashboard/projects.tsx
@@ -23,7 +23,7 @@ export default function Projects(){
                 <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Projects</h3>
                 {/* manipulate ai card */}
                 <div className='flex items-center content-center justify-center w-full text-center z-10'>
-                    <Card className='w-1/2 m-10 justify-between border-accent-border border-2 hover:drop-shadow-xs'>
+                    <Card className='w-1/2 m-10 justify-between border-accent-border border-2 hover:drop-shadow-xs hover:-translate-y-4 transition-translate duration-300 ease-in-out'>
                         <CardContent className='flex pt-6 pr-6 pl-6 content-center justify-center'>
                             <img src='/images/manipulateai.png'width={600} className='border-accent-border border-2'/>
                         </CardContent>
@@ -46,21 +46,23 @@ export default function Projects(){
                 </div>
                 {/* hugobot card */}
                 <div className='flex items-center content-center justify-center w-full text-center z-10'>
-                    <Card className='w-1/2 m-10 mt-0 justify-between border-accent-border border-2 hover:drop-shadow-xs'>
+                    <Card className='w-1/2 m-10 mt-0 justify-between border-accent-border border-2 hover:drop-shadow-xs hover:-translate-y-4 transition-translate duration-300 ease-in-out will-change-transform'>
                         <CardContent className='flex pt-6 pr-6 pl-6 content-center justify-center'>
                             {isClient && <ReactPlayer url='https://youtu.be/6mUZqZc3WWk' width={600} className='border-accent-border border-2' />}
                         </CardContent>
-                        <CardHeader className='flex pr-6 pl-6 pb-6 pt-0 justify-center text-center'>
-                            <CardTitle className='flex justify-center text-center'><a href="https://github.com/hugo-mp3/HugoBot-frontend" className='flex ml-[1.6rem]'>HugoBot<WiDirectionUpRight className='h-[1.6rem] w-[1.6rem] -mt-1'/></a></CardTitle>
-                            <CardDescription className='text-primary'>An interactive 3D virtual companion app</CardDescription>
-                            <div className="flex flex-wrap h-5 items-center justify-center space-x-4 text-xs text-accent-border">
-                                <div>React.js</div>
-                                <div>Node.js</div>
-                                <div>Express.js</div>
-                                <div>Chakra UI</div>
-                                <div>Vite</div>
-                                <div>TailwindCSS</div>
-                                <div>OpenAI</div>
+                        <CardHeader className='flex flex-col items-center pr-6 pl-6 pb-6 pt-0 justify-center text-center'>
+                            <CardTitle className='flex justify-center text-center -mb-1'><a href="https://github.com/hugo-mp3/HugoBot-frontend" className='flex ml-[1.6rem]'>HugoBot<WiDirectionUpRight className='h-[1.6rem] w-[1.6rem] -mt-1'/></a></CardTitle>
+                            <div className="flex flex-col justify-center items-center h-full">
+                                <CardDescription className='text-primary mb-1'>An interactive 3D virtual companion app</CardDescription>
+                                <div className="flex flex-wrap h-5 items-center justify-center space-x-4 text-xs text-accent-border">
+                                    <div>React.js</div>
+                                    <div>Node.js</div>
+                                    <div>Express.js</div>
+                                    <div>Chakra UI</div>
+                                    <div>Vite</div>
+                                    <div>TailwindCSS</div>
+                                    <div>OpenAI</div>
+                                </div>
                             </div>
                         </CardHeader>
                     </Card>
