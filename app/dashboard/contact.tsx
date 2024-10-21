96fead256938526e918e3012bedaeb01b4cf83ce
Ale <alejaman04@gmail.com>
2024-10-21 19:24:08 -0400

    created contact component

---
+++ app/dashboard/contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function Contact(){
+    return(
+        <>
+            <div className='w-full text-center z-10'>
+                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Contact</h3>
+                    
+            </div>
+        </>
+    )
+}
