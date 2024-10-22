19f6467493ee20160d0c081e059379400edec94c
Ale <alejaman04@gmail.com>
2024-10-22 12:24:05 -0400

    got rid of background animation and related imports as well as replaced headshot

--- app/dashboard/aboutme.tsx
+++ app/dashboard/aboutme.tsx
@@ -1,25 +1,14 @@
-"use client"
 import React from 'react';
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 
 export default function AboutMe(){
     return(
         <>
-            {/* New DotLottie animation implementation */}
-            <div className='flex -left-44 w-1/2 absolute h-2/5 -mt-28 overflow-hidden'>
-                <DotLottieReact
-                    src="https://lottie.host/860fd3ec-ccc3-474c-87c6-927abb6aed3c/UVOirpsQt8.lottie"
-                    loop
-                    autoplay
-                    /* style={{ height: '400px', width: '100%' }} */
-                    className='z-0 object-cover border-b-2 border-accent'
-                />
-            </div>
+            
             <div className='z-10 relative text-center pt-14'>
                 <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">About Me</h3>
-                <div className='flex'>
-                    <img src='/images/headshot.png'width={200} className='ml-10 -mt-11'/>
+                <div className='flex justify-center items-center'>
+                    <img src='/images/newheadshot.png' className='inline ml-3 w-1/6 h-1/6'/>
                     <p className="leading-7 [&:not(:first-child)]:mt-6 inline m-5 mr-28">
                         Software Engineer and Web Developer with experience as a 
                         <span className="leading-7 [&:not(:first-child)]:mt-6 inline text-accent-hover"> Research Intern </span>
