edb50f8440c369c1e873be3e835c4e18c42cd2f5
Ale <alejaman04@gmail.com>
2024-10-24 16:23:50 -0400

    removed research intern and linked medstar health research institute

--- app/dashboard/aboutme.tsx
+++ app/dashboard/aboutme.tsx
@@ -10,11 +10,9 @@ export default function AboutMe(){
                 <div className='flex justify-center items-center'>
                     <img src='/images/newheadshot.png' className='inline ml-3 w-1/6 h-1/6'/>
                     <p className="leading-7 [&:not(:first-child)]:mt-6 inline m-5 mr-28">
-                        Software Engineer and Web Developer with experience as a 
-                        <span className="leading-7 [&:not(:first-child)]:mt-6 inline text-accent-hover font-bold"> Research Intern </span>
-                        at 
-                        <span className="leading-7 [&:not(:first-child)]:mt-6 inline text-accent-hover font-bold"> Medstar Health Research Institute</span>, 
-                        with a strong focus on continuous learning and hands-on project development. Currently pursuing academic excellence while 
+                        Software Engineer and Web Developer with experience at 
+                        <a href="https://www.medstarhealth.org/innovation-and-research/medstar-health-research-institute"><span className="leading-7 [&:not(:first-child)]:mt-6 inline text-accent-hover font-bold"> Medstar Health Research Institute</span></a>. 
+                        I have a strong focus on continuous learning and hands-on project development. Currently pursuing academic excellence while 
                         actively building a solid foundation in software engineering principles. Eager to contribute to innovative teams, solve 
                         complex problems, and grow into a role where I can make a meaningful impact on product development and people.
                     </p>
