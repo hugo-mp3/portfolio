fbc3c4cb7c3b49f1cc01e99f1f0201cc3abbdbeb
Ale <alejaman04@gmail.com>
2024-10-24 16:32:06 -0400

    made download resume button work

--- app/dashboard/intro.tsx
+++ app/dashboard/intro.tsx
@@ -14,9 +14,11 @@ export default function Intro(){
                     <p className="leading-7 [&:not(:first-child)]:mt-6 text-muted-foreground">Howdy! I am </p>
                     <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-primary">Hugo Manrique-Pinell</h1>
                     <h2 className="scroll-m-20 w-30 pb-2 text-3xl font-semibold tracking-tight first:mt-0">Full Stack Developer</h2>
-                    <Button>
-                        <ReaderIcon className="mr-2 h-4 w-4" /> Download Resume
-                    </Button>
+                    <a href="/HugompCV.pdf" download="Hugo_Resume.pdf">
+                        <Button>
+                            <ReaderIcon className="mr-2 h-4 w-4" /> Download Resume
+                        </Button>
+                    </a>
                 </div>
                 {/* New DotLottie animation implementation */}
                 <DotLottieReact
