4663876c1a7b09b630b8fc43ee58ff9fc17f6648
Ale <alejaman04@gmail.com>
2024-10-25 17:23:43 -0400

    made intro responsive

--- app/dashboard/intro.tsx
+++ app/dashboard/intro.tsx
@@ -18,10 +18,10 @@ const popUpVariants = {
 export default function Intro() {
     return (
         <>
-            <div className="flex items-center justify-start space-x-10 pt-12">
-                <div className="ml-10 pb-10">
+            <div className="flex w-screen items-center justify-start base:space-x-0 sm:space-x-10 base:pt-6 sm:pt-12">
+                <div className="base:ml-2 sm:ml-10 base:pb-0 sm:pb-10">
                     <motion.p
-                        className="leading-7 [&:not(:first-child)]:mt-6 text-muted-foreground ml-1"
+                        className="leading-7 [&:not(:first-child)]:mt-6 text-muted-foreground ml-1 base:text-sm sm:text-base"
                         initial="hidden"
                         whileInView="visible"
                         viewport={{ amount: 0.2 }}
@@ -31,7 +31,7 @@ export default function Intro() {
                         Howdy! I am
                     </motion.p>
                     <motion.h1
-                        className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-primary"
+                        className="scroll-m-20 base:text-3xl sm:text-4xl font-extrabold tracking-tight lg:text-5xl text-primary"
                         initial="hidden"
                         whileInView="visible"
                         viewport={{amount: 0.2 }}
@@ -41,7 +41,7 @@ export default function Intro() {
                         Hugo Manrique-Pinell
                     </motion.h1>
                     <motion.h2
-                        className="scroll-m-20 w-30 pb-2 text-3xl font-semibold tracking-tight first:mt-0"
+                        className="scroll-m-20 w-30 pb-2 base:text-2xl sm:text-3xl font-semibold tracking-tight first:mt-0"
                         initial="hidden"
                         whileInView="visible"
                         viewport={{amount: 0.2 }}
@@ -59,8 +59,8 @@ export default function Intro() {
                         transition={{ duration: 0.5, delay: 0.9 }}
                         variants={itemVariants}
                     >
-                        <Button>
-                            <ReaderIcon className="mr-2 h-4 w-4" /> Download Resume
+                        <Button className='base:w-36 base:h-7 base:text-xs sm:w-44 sm:h-9 sm:text-sm'>
+                            <ReaderIcon className="base:mr-0 sm:mr-2 h-4 w-4" /> Download Resume
                         </Button>
                     </motion.a>
                 </div>
@@ -71,12 +71,14 @@ export default function Intro() {
                     viewport={{ amount: 0.2 }}
                     transition={{ duration: 0.5, delay: 0.5 }}
                     variants={popUpVariants}
+                    className="base:mt-16 sm:mt-0"
                 >
                     <DotLottieReact
                         src="https://lottie.host/3bc940a1-acb5-4560-8cd5-c6ed3809eaa3/T8tlf8WyPI.json"
                         loop
                         autoplay
-                        style={{ height: '300px', width: '300px' }}
+                        /* style={{ height: '300px', width: '300px' }} */
+                        className='base:w-52 base:h-52 sm:w-80 sm:h-80'
                     />
                 </motion.div>
             </div>
