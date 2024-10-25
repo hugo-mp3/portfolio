ed736a72f43b7269eda09d653462a1c7351c6356
Ale <alejaman04@gmail.com>
2024-10-25 17:26:09 -0400

    made navbar responsive and centered better

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -52,37 +52,37 @@ export default function Navbar() {
   return (
     <>
       <div
-        className={`fixed w-full content-center text-center bg-background space-x-4 justify-center z-50 pt-2 transition-transform duration-300 ${
+        className={`fixed w-screen ml-[1rem] flex justify-self-center items-center content-center text-center bg-background space-x-4 justify-center z-50 pt-2 transition-transform duration-300 ${
           visible ? "transform translate-y-0" : "transform -translate-y-full"
         }`}
       >
         <h4
-          className="inline scroll-m-20 text-xl font-semibold tracking-tight hover:text-accent-hover cursor-pointer"
+          className="inline scroll-m-20 base:text-base sm:text-xl font-semibold tracking-tight hover:text-accent-hover cursor-pointer"
           onClick={() => scrollToSection("home")}
         >
           home
         </h4>
         <h4
-          className="inline scroll-m-20 text-xl font-semibold tracking-tight hover:text-accent-hover cursor-pointer"
+          className="inline scroll-m-20 base:text-base sm:text-xl font-semibold tracking-tight hover:text-accent-hover cursor-pointer"
           onClick={() => scrollToSection("about")}
         >
           about me
         </h4>
         <h4
-          className="inline scroll-m-20 text-xl font-semibold tracking-tight hover:text-accent-hover cursor-pointer"
+          className="inline scroll-m-20 base:text-base sm:text-xl font-semibold tracking-tight hover:text-accent-hover cursor-pointer"
           onClick={() => scrollToSection("projects")}
         >
           projects
         </h4>
         <h4
-          className="inline scroll-m-20 text-xl font-semibold tracking-tight hover:text-accent-hover cursor-pointer"
+          className="inline scroll-m-20 base:text-base sm:text-xl font-semibold tracking-tight hover:text-accent-hover cursor-pointer"
           onClick={() => scrollToSection("contact")}
         >
           contact
         </h4>
-        <Button variant="outline" size="icon" onClick={() => handleButtonPress()}>
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="outline" className='items-center justify-center base:h-7 base:w-7 sm:h-9 sm:w-9 ' onClick={() => handleButtonPress()}>
+          <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </div>
