a71377c361d4196ca7e3e467c0f5fb685dab10e7
Ale <alejaman04@gmail.com>
2024-11-25 21:21:24 -0500

    added IoT project

--- app/dashboard/projects.tsx
+++ app/dashboard/projects.tsx
@@ -74,7 +74,7 @@ export default function Projects() {
                                 </a>
                             </CardTitle>
                             <CardDescription className="text-primary base:text-xs sm:text-sm">
-                                A platform and SaaS solution that provides users with a personalized AI mentor for guidance and manipulation.
+                                A platform and SaaS solution that provides users with a personalized AI mentor for guidance and manipulation
                             </CardDescription>
                             <div className="flex flex-wrap h-7 items-center justify-center space-x-4 base:text-xxs sm:text-xs text-accent-border">
                                 <div>Next.js</div>
@@ -121,6 +121,39 @@ export default function Projects() {
                                     <div>Vite</div>
                                     <div>TailwindCSS</div>
                                     <div>OpenAI</div>
+                                </div>
+                            </div>
+                        </CardHeader>
+                    </Card>
+                </motion.div>
+                {/* light IoT card */}
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ amount: 0.2 }}
+                    transition={{ duration: 0.5, delay: 0.3 }}
+                    variants={itemVariants}
+                    className="flex items-center content-center justify-center w-full text-center z-10"
+                >
+                    <Card className="base:w-10/12 sm:w-3/4 lg:w-1/2 base:m-0 base:mb-10 sm:m-10 sm:mt-6 justify-between border-accent-border border-2 hover:drop-shadow-xs hover:-translate-y-3 transition-translate duration-300 ease-in-out will-change-transform">
+                        <CardContent className="flex base:pb-3 sm:pb-6 pt-6 pr-6 pl-6 content-center justify-center">
+                            <YouTubeEmbed videoId="kRta1x8B25g" />
+                        </CardContent>
+                        <CardHeader className="flex flex-col items-center pr-6 pl-6 pb-6 pt-0 justify-center text-center">
+                            <CardTitle className="flex justify-center text-center -mb-1 text-2xl text-accent-hover underline">
+                                <a href="https://github.com/hugo-mp3/lightIOT-ReactNative-Expo-App" className="flex ml-[2.2rem]">
+                                    IoT Light Control System<VscGithub className="h-[1.8rem] w-[1.8rem] ml-[0.4rem]" />
+                                </a>
+                            </CardTitle>
+                            <div className="flex flex-col justify-center items-center h-full">
+                                <CardDescription className="text-primary mb-1 base:text-xs sm:text-sm">
+                                    Built a mobile app to control a light via an ESP32 microcontroller hosting an HTTP server over a WiFi access point 
+                                </CardDescription>
+                                <div className="flex flex-wrap h-7 items-center justify-center space-x-4 base:text-xxs sm:text-xs text-accent-border">
+                                    <div>React Native</div>
+                                    <div>Expo</div>
+                                    <div>TypeScript</div>
+                                    <div>C++</div>
                                 </div>
                             </div>
                         </CardHeader>
